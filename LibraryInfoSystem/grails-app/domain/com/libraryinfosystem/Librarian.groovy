d7e7ec0ea18e989177caa7d5b0f06f89c03d0852
Morphoo <jfboyce@gmail.com>
2015-04-22 21:36:23 +0100

    First Commit - Blank App

---
+++ LibraryInfoSystem/grails-app/domain/com/libraryinfosystem/Librarian.groovy
@@ -0,0 +1,18 @@
+package com.libraryinfosystem
+
+class Librarian {
+
+ String name
+ String email
+ String userName
+ String password
+ String telephone
+ 
+ 
+ String toString(){
+ "$name, $email, $userName, $password, $telephone"
+ }
+
+    static constraints = {
+    }
+}
